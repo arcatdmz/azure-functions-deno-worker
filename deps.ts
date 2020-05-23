9e4e01e8dbf805a78f723f40d38374494dd4b7f1
Jun Kato <i@junkato.jp>
2020-05-24 05:23:06 +0900

    Update deps to catch up with Deno > v1.0.1

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parse } from "https://deno.land/std@0.51.0/flags/mod.ts";
-export { readZip } from "https://deno.land/x/jszip@0.6.0/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.51.0/fs/ensure_dir.ts";
-export { move } from "https://deno.land/std@0.51.0/fs/move.ts";
-export { walk } from "https://deno.land/std@0.51.0/fs/walk.ts";
-export { Application, Router, Context as OakContext, Body } from "https://deno.land/x/oak@v4.0.0/mod.ts";
+export { parse } from "https://deno.land/std@0.53.0/flags/mod.ts";
+export { readZip } from "https://cdn.jsdelivr.net/gh/arcatdmz/deno-zip@6bdeff81c73978fdc49f317539cfd54df0bab55f/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.53.0/fs/ensure_dir.ts";
+export { move } from "https://deno.land/std@0.53.0/fs/move.ts";
+export { walk } from "https://deno.land/std@0.53.0/fs/walk.ts";
+export { Application, Router, Context as OakContext, Body } from "https://cdn.jsdelivr.net/gh/oakserver/oak@ca212615989ddf7376858e933f6a0a69c1b09478/mod.ts";
